413419af192777d611926dbf89517162906254ad
Karol <karol.rybski04@gmail.com>
2023-04-28 07:29:54 +0200

    update

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import yaml
 startup_extensions = ["music"]
 
 client = commands.Bot(
+    intents=discord.Intents.default(),
     command_prefix='>',
     help_command=None,
     case_insensitive=True
@@ -22,24 +23,24 @@ async def on_ready():
     print("Im Ready!")
     for extension in startup_extensions:
         try:
-            client.load_extension(extension)
+            await client.load_extension(extension)
         except Exception as e:
             print(e)
 
 
 ### ----------------------------------- KOMENDY DLA WSZYSTKICH ----------------------------------- ###
-@client.command()
+@client.tree.command(description='Sprawdź opóźnienie bota! ', guild=discord.Object('619864487209730048'))
 async def ping(ctx):
-    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+    await ctx.response.send_message(f"Pong! {round(client.latency * 1000)}ms")
 
-@client.command()
+@client.tree.command(description='Wyświetl pomocne komendy ', guild=discord.Object('619864487209730048'))
 async def help(ctx):
     global embed
     embed.set_image(url="")
     embed.set_author(name='Aktualne komendy!')
 
-    embed.add_field(name=">help", value="Wyświetla informacje o komendach")
-    embed.add_field(name=">ping", value="Pokazuje czas reakcji bota")
+    embed.add_field(name="/help", value="Wyświetla informacje o komendach")
+    embed.add_field(name="/ping", value="Pokazuje czas reakcji bota")
     embed.add_field(name=">clear {liczba}", value="Usuwa podaną liczbę wiadomości od dołu (tylko ranga Mems)")
     await ctx.send(f"Cześć! {ctx.message.author.mention} Oto co mogę zrobić dla ciebie: ", embed=embed)
 
@@ -48,9 +49,9 @@ async def help(ctx):
 
 
 ### ----------------------------------- KOMENDY RANGI ADMIN ----------------------------------- ###
-@client.command()
+@client.tree.command(description='Usuń podaną ilość wiadomości ', guild=discord.Object('619864487209730048'))
 @commands.has_role('ADMIN')
-async def clear(ctx, number):
+async def clear(ctx, number: int):
     await ctx.channel.purge(limit=int(number) + 1)
 
 
--- music.py
+++ music.py
@@ -9,6 +9,7 @@ import random
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import yaml
+import asyncio
 
 with open("tokens.yaml", "r") as stream:
     tokens = yaml.safe_load(stream)
@@ -46,8 +47,8 @@ class MusicCog(commands.Cog):
         message = ""
         for index, x in enumerate(queue):
             message += f"{index + 1}. {x[1]} \n"
-        kanal = await self.client.get_channel(int(kanalmid)).history(limit=2).flatten()
-        for index, mess in enumerate(kanal):
+        kanal = [message async for message in self.client.get_channel(int(kanalmid)).history(limit=2)]
+        for mess in kanal:
             if int(mess.id) == kanalw:
                  await mess.edit(content="**Kolejka: \n**" + message)
 
@@ -55,8 +56,9 @@ class MusicCog(commands.Cog):
         global nowplaying
         global queue
 
-        kanal = await self.client.get_channel(int(kanalmid)).history(limit=2).flatten()
-        for index, mess in enumerate(kanal):
+        kanal = [message async for message in self.client.get_channel(int(kanalmid)).history(limit=2)]
+        
+        for mess in kanal:
             if int(mess.id) == kanalw:
                 try:
                     url = f"https://img.youtube.com/vi/{nowplaying[2]}/0.jpg"
@@ -96,113 +98,113 @@ class MusicCog(commands.Cog):
                     pass
                 with YoutubeDL(YDL_OPTIONS) as ydl:
                     ydl.download([queue[0][0]])
-                    voice.play(discord.FFmpegPCMAudio(source='audioFile.mp3'), after=self.nextsong)
+                    voice.play(discord.FFmpegPCMAudio(source='audioFile.mp3', executable="C:/ffmpeg/bin/ffmpeg.exe"), after=self.nextsong)
                     voice.is_playing()
                     self.client.loop.create_task(self.editkolej())
                     queue.remove(queue[0])
             else:
                 return
 
+
     @commands.Cog.listener()
     async def on_message(self, message):
-        try:
-            if message.channel.id == int(kanalmid) and str(message.author) != "Stefano#5343":
-                global voice
-                global queue
-                global nowplaying
-                global loop
-                channel = self.client.get_channel(int(kanalmid))
-                await channel.purge(limit=1)
+        print(message.content)
+        if message.channel.id == int(kanalmid) and str(message.author) != "Stefano#5343":
+            global voice
+            global queue
+            global nowplaying
+            global loop
+            channel = self.client.get_channel(int(kanalmid))
+            await channel.purge(limit=1)
+            try:
+                if voice.is_connected():
+                    pass
+            except:
+                guild = message.author.guild
+                VoiceChannel = message.author.voice.channel
+                await VoiceChannel.connect()
+                voice = discord.utils.get(self.client.voice_clients, guild=guild)
+            if message.content.lower() == "leave":
                 try:
-                    if voice.is_connected():
-                        pass
-                except:
-                    guild = message.author.guild
-                    VoiceChannel = message.author.voice.channel
-                    await VoiceChannel.connect()
-                    voice = discord.utils.get(self.client.voice_clients, guild=guild)
-                if message.content.lower() == "leave":
-                    try:
-                        await voice.disconnect()
-                        queue = []
-                        nowplaying = ""
-                        voice = None
-                        await self.editkolej()
-                        await self.editaktu()
-                    except:
-                        await channel.send("Nie mogę wyjść z kanału", delete_after=3)
-
-                elif message.content.lower() == "pause":
-                    try:
-                        await voice.pause()
-                    except:
-                        await channel.send("Piosenka nie może być zatrzymana", delete_after=3)
-
-                elif message.content.lower() == "resume":
-                    try:
-                        await voice.resume()
-                    except:
-                        await channel.send("Piosenka nie może być wznowiona", delete_after=3)
-
-                elif message.content.lower() == "skip":
-                    try:
-                        await voice.stop()
-                    except:
-                        pass
-                elif message.content[0:6].lower() == "remove":
-                   try:
-                       queue.remove(queue[int(message.content[7:])-1])
-                       await self.editkolej()
-                       await self.editaktu()
-                   except:
-                       await channel.send("Ta pozycja nie może być usunięta", delete_after=3)
-                elif message.content.lower() == "shuffle":
-                    try:
-                        random.shuffle(queue)
-                        await self.editkolej()
-                    except:
-                        await channel.send("Kolejka nie może być pomieszana", delete_after=3)
-                elif message.content.lower() == "clear":
-                    try:
-                        queue = []
-                        nowplaying = ""
-                        voice = None
-                        await self.editkolej()
-                        await self.editaktu()
-                    except:
-                        await channel.send("Nie mozna wyczyscic kolejki", delete_after=3)
-                elif message.content[0:4].lower() == "move":
-                    try:
-                        pos1, pos2 = (int(message.content[5])-1), (int(message.content[7])-1)
-                        temp = queue[pos1]
-                        queue[pos1] = queue[pos2]
-                        queue[pos2] = temp
-                        
-                        await self.editkolej()
-                    except:
-                        await channel.send("Nie mozna zamienic piosenek", delete_after=3)
-                else:
-                    if message.content[0:22] == "https://soundcloud.com":
-                        url = message.content
-                    else:
-                        if message.content[0:30] == "https://open.spotify.com/track":
-                            info = sp.track(message.content[31:53])
-                            message.content = u' '.join((info['name'], info['artists'][0]['name'])).encode('ascii', 'ignore').decode('ascii')
-                        html = urllib.request.urlopen("https://www.youtube.com/results?search_query="+ message.content.replace(" ", "+"))
-                        video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-                        url = "https://www.youtube.com/watch?v=" + video_ids[0]
-
-                    with YoutubeDL(YDL_OPTIONS) as ydl:
-                        info = ydl.extract_info(url, download=False)
-                        video_title = info.get('title', None)
-                        video_thumbnail = info.get('id', None)
-
-                    queue.append([str(url), video_title, video_thumbnail])
-
-                    self.nextsong()
+                    await voice.disconnect()
+                    queue = []
+                    nowplaying = ""
+                    voice = None
                     await self.editkolej()
-        except Exception as e:
-            print(e)
+                    await self.editaktu()
+                except:
+                    await channel.send("Nie mogę wyjść z kanału", delete_after=3)
+
+            elif message.content.lower() == "pause":
+                try:
+                    await voice.pause()
+                except:
+                    await channel.send("Piosenka nie może być zatrzymana", delete_after=3)
+
+            elif message.content.lower() == "resume":
+                try:
+                    await voice.resume()
+                except:
+                    await channel.send("Piosenka nie może być wznowiona", delete_after=3)
+
+            elif message.content.lower() == "skip":
+                try:
+                    await voice.stop()
+                except:
+                    pass
+            elif message.content[0:6].lower() == "remove":
+                try:
+                    queue.remove(queue[int(message.content[7:])-1])
+                    await self.editkolej()
+                    await self.editaktu()
+                except:
+                    await channel.send("Ta pozycja nie może być usunięta", delete_after=3)
+            elif message.content.lower() == "shuffle":
+                try:
+                    random.shuffle(queue)
+                    await self.editkolej()
+                except:
+                    await channel.send("Kolejka nie może być pomieszana", delete_after=3)
+            elif message.content.lower() == "clear":
+                try:
+                    queue = []
+                    nowplaying = ""
+                    voice = None
+                    await self.editkolej()
+                    await self.editaktu()
+                except:
+                    await channel.send("Nie mozna wyczyscic kolejki", delete_after=3)
+            elif message.content[0:4].lower() == "move":
+                try:
+                    pos1, pos2 = (int(message.content[5])-1), (int(message.content[7])-1)
+                    temp = queue[pos1]
+                    queue[pos1] = queue[pos2]
+                    queue[pos2] = temp
+                    
+                    await self.editkolej()
+                except:
+                    await channel.send("Nie mozna zamienic piosenek", delete_after=3)
+            else:
+                if message.content[0:22] == "https://soundcloud.com":
+                    url = message.content
+                else:
+                    if message.content[0:30] == "https://open.spotify.com/track":
+                        info = sp.track(message.content[31:53])
+                        message.content = u' '.join((info['name'], info['artists'][0]['name'])).encode('ascii', 'ignore').decode('ascii')
+                    html = urllib.request.urlopen("https://www.youtube.com/results?search_query="+ message.content.replace(" ", "+"))
+                    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+                    print(video_ids)
+                    url = "https://www.youtube.com/watch?v=" + video_ids[0]
+
+                with YoutubeDL(YDL_OPTIONS) as ydl:
+                    info = ydl.extract_info(url, download=False)
+                    video_title = info.get('title', None)
+                    video_thumbnail = info.get('id', None)
+
+                queue.append([str(url), video_title, video_thumbnail])
+
+                self.nextsong()
+                await self.editkolej()
 
     @commands.command()
     async def musicsetup(self, ctx):
@@ -234,7 +236,7 @@ class MusicCog(commands.Cog):
             embed.set_image(url="")
             await channel.send('**Kolejka:\n**', embed=embed)
             kanal = self.client.get_channel(int(kanalmid))
-            kanal = await kanal.history(limit=2).flatten()
+            kanal = await kanal.history(limit=2)
             file.write(str(kanal[0].id) + "\n")
             kanalw = kanal[0].id
 
@@ -242,5 +244,5 @@ class MusicCog(commands.Cog):
             await ctx.send("Kanał już istnieje!")
 
 
-def setup(client):
-    client.add_cog(MusicCog(client))
+async def setup(client):
+    await client.add_cog(MusicCog(client))
